df9e82e56be27e43c7ee33ccf9859b6219fa2d8e
Elena Vasilevskaya <elenavasilevskaya@MacBook-Air-Elena.local>
2025-03-13 15:15:54 +0100

    update1

--- models/staging/staging_airports.sql
+++ models/staging/staging_airports.sql
@@ -3,7 +3,7 @@
 WITH airports_regions_join AS (
 SELECT * 
 FROM {{source('flights_data', 'airports')}}
-LEFT JOIN {{source('flights_data', 'regions')}}
+LEFT JOIN {{source('flights_data', 'public.regions')}}
 USING (country)
 )
 SELECT * FROM airports_regions_join
